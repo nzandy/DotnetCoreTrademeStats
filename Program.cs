d19efb3c12b1e0d82a847a740e265d9eaea1b91f
scarfie <andydaviesnz@gmail.com>
2017-02-19 15:45:56 +1300

    A working .NET Core App, with 2 models that are mapped to Entity Framework NpgSQL

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ConsoleApplication
+namespace DotnetCoreTrademeStats
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
