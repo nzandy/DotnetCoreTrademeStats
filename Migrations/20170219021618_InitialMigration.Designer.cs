1d114d4240b4835f3602afca32d7743340ca58e9
scarfie <andydaviesnz@gmail.com>
2017-02-19 15:47:48 +1300

    A working .NET Core App, with 2 models that are mapped to Entity Framework NpgSQL 2

---
+++ Migrations/20170219021618_InitialMigration.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetCoreTrademeStats.Migrations
+{
+    [DbContext(typeof(ArticleContext))]
+    [Migration("20170219021618_InitialMigration")]
+    partial class InitialMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("Article", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AuthorId");
+
+                    b.Property<string>("Body");
+
+                    b.Property<string>("Title");
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AuthorId");
+
+                    b.ToTable("Articles");
+                });
+
+            modelBuilder.Entity("Author", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Authors");
+                });
+
+            modelBuilder.Entity("Article", b =>
+                {
+                    b.HasOne("Author", "Author")
+                        .WithMany("Articles")
+                        .HasForeignKey("AuthorId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ Models/ArticleContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ArticleContext : DbContext {
+	public ArticleContext(DbContextOptions<ArticleContext> options) : base (options){ 
+		
+	 }
+
+	public DbSet<Article> Articles { get; set; }
+	public DbSet<Author> Authors { get; set; }
+}
