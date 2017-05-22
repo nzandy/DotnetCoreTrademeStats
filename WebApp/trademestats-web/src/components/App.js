d27878976ac6868ec3a26e67d133107a7ba065d1
scarfie <andydaviesnz@gmail.com>
2017-05-22 15:46:46 +0800

    Introduce React Router to route requests to components.
    Add basic home page.

--- WebApp/trademestats-web/src/components/App.js
+++ WebApp/trademestats-web/src/components/App.js
@@ -1,17 +1,32 @@
 var React = require('react');
-import '../App.css';
+var Home = require('./Home');
+require('../App.css');
 var RentalListings = require('./RentalListings');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 
 class App extends React.Component {
 	render() {
 		return (
+			<Router>
 			<div className="container">
 				<div className="App-header">
 					<h2>House Rentals NZ.</h2>
 				</div>
-				<RentalListings/> 
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/rentals' component={RentalListings} />
+					<Route render={function (){
+							return (
+								<p> Not Found </p>
+							)
+						}} />
+				</Switch>
 			</div>
+			</Router>
 		)
 	}
 }
---
+++ WebApp/trademestats-web/src/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Home extends React.Component{
+	render(){
+		return(
+			<div>
+				<p> Welcome to the Trademe Stats App. </p>
+			</div>
+		)
+	}
+}
+
+module.exports = Home;
