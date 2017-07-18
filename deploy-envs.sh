3504e4b49eedaae8ec1a44bf87df79bd1f74588a
scarfie <andydaviesnz@gmail.com>
2017-07-18 12:30:08 +0800

    Try updated AWS region

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -2,6 +2,6 @@
 export IMAGE_NAME=tmswebapi
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-2b
+export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=rentalstats.co.nz
